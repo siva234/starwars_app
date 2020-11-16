871198c456a49d7ef4b4784eb2a35d4fa342080d
siva reddy <siva.agent@gmail.com>
2020-11-16 01:03:21 +0100

    all tasks done CSS remains

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Movielist from './components/MovieList/MovieList';
 import SideDisplay from './components/SideDisplay/SideDisplay';
-import Grid from '@material-ui/core/Grid';
-import { Button, Input } from '@material-ui/core';
+import {Grid, Button, Menu, MenuItem, Input} from '@material-ui/core';
 
 function App() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const [filmsData, setFilmsData] = useState([]);
   const [displayData, setDisplayData] = useState([]);
   const [searchWord, setSearchWord] = useState("");
@@ -14,35 +15,87 @@ function App() {
     fetch('https://star-wars-api.herokuapp.com/films')
       .then(response => response.json())
       .then(data => setFilmsData(data));
-    console.log("SearchWord".searchWord);
   }, [])
 
+  const handleSortMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSortMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleSortMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleMovieClick = (value) =>{
-    console.log(value);
     setDisplayData(value);
   }
-  let handleSearchChange = event => {
-    let val = {value: event.target.value};
-    console.log("SearchWord".val);
+
+  useEffect(() => {
+    let sorted = [...filmsData];
+    if(selectedIndex===1){
+      sorted = [...filmsData].sort((a, b) => {
+        if(a.fields.episode_id < b.fields.episode_id)
+          return -1;
+        if(a.fields.episode_id > b.fields.episode_id)
+          return 1;
+        return 0;
+      });
+    }
+    if(selectedIndex===2){
+      sorted = [...filmsData].sort((a, b) => {
+        if(a.fields.release_date < b.fields.release_date)
+          return -1;
+        if(a.fields.release_date > b.fields.release_date)
+          return 1;
+        return 0;
+      });
+    }
+    setFilmsData(sorted);
+  }, [selectedIndex])
+
+  const handleSearchChange = (event) => {
+    let val = event.target.value;
     setSearchWord(val);
-  };
+  }
   
   return (
     <Grid container justify="center" className="App" spacing={2}>
       <Grid item xs={12} className="TopBar">
         <Grid container justify="center" spacing={2} >
-          <Grid item className="SortMenu"></Grid>
-          <Grid item className="SearchBox"></Grid>
+          <Grid item xs={4} className="SortMenuContainer">
+            <div>
+              <Button className="SortMenuBtn" aria-controls="simple-menu" aria-haspopup="true" onClick={handleSortMenuClick}>
+                Sort by...
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleSortMenuClose}
+              >
+                <MenuItem onClick={(event) => handleSortMenuItemClick(event, 1)}>Episode</MenuItem>
+                <MenuItem onClick={(event) => handleSortMenuItemClick(event, 2)}>Year</MenuItem>
+              </Menu>
+            </div>
+          </Grid>
+          <Grid item xs={8} className="SearchBoxContainer">
+            <Input className="SearchBox" type="text" value={searchWord.value} onChange={handleSearchChange}/>
+          </Grid>
         </Grid>
       </Grid>
       <Grid item xs={12} className="DisplayBoxes">
         <Grid container justify="center" spacing={2}>
-            <Grid item xs={5} className="MovieList">
-              {filmsData.filter(film => film.fields.title.includes({searchWord}) || !searchWord).map(obj =>                
-                  <a className="MovieListItems" onClick={() => handleMovieClick(obj.fields)}><Movielist key={obj.fields.id} filmobj={obj.fields}  /></a>
+            <Grid item xs={5} className="MovieListContainer">
+              {filmsData.filter(film => film.fields.title.toLowerCase().includes(searchWord.toLowerCase()) || !searchWord).map(obj => 
+                  <Button key={obj.id} className="MovieListItems" onClick={() => handleMovieClick(obj.fields)}><Movielist filmobj={obj.fields}  /></Button>
               )}
             </Grid>
-            <Grid item xs={5} className="SideDisplay">  
+            <Grid item xs={5} className="SideDisplayContainer">  
               <SideDisplay displayData={displayData} />
             </Grid>
           </Grid>
--- src/components/SideDisplay/SideDisplay.js
+++ src/components/SideDisplay/SideDisplay.js
@@ -3,15 +3,23 @@ import styles from './SideDisplay.module.css';
 
 function SideDisplay({displayData}) {
     return (
-        <div>
-            <h1>{displayData.title}</h1>
-            <p className={styles.desc_para}>
-                {displayData.opening_crawl}<br/><br/>
-                Directed by: {displayData.director}<br/><br/>
-                Produced by: {displayData.producer}<br/><br/>
-                Releaased on: {displayData.release_date}
-            </p>
-        </div>
+        <>
+        {(displayData?.title)?(
+            <div>
+                <h1>{displayData.title}</h1>
+                <p className={styles.desc_para}>
+                    {displayData.opening_crawl}<br/><br/>
+                    Directed by: {displayData.director}<br/><br/>
+                    Produced by: {displayData.producer}<br/><br/>
+                    Releaased on: {displayData.release_date}
+                </p>
+            </div>
+        ):(
+            <div>
+                No movie Selected
+            </div>
+        )}
+        </>
     )
 }
 
